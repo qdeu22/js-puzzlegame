09ce7bd7b7da6e127984bc8679c039df5987df3c
qdeu22 <qdeu22@gmail.com>
2023-04-15 23:58:28 +0900

    feat: Array(tileCount)   .fill()   .forEach() 를 이용해 배열을 만들고 쉽게 배열지원

--- main.js
+++ main.js
@@ -2,3 +2,17 @@ const container = document.querySelector(".image-container");
 const startButton = document.querySelector(".start-button");
 const gameText = document.querySelector(".game-text");
 const playTime = document.querySelector(".play-time");
+
+const tileCount = 16;
+
+// for (var i = 0; i < 16; i++){
+
+// }
+
+//Array(tileCount).fill(); //16개 짜리 배열 값은 안들어 있음..
+
+Array(tileCount)
+  .fill()
+  .forEach((_, i) => {
+    console.log(i);
+  });
