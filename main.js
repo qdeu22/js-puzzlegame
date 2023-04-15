78267682165ad97d5c56b3ebdcd1819875a7a022
qdeu22 <qdeu22@gmail.com>
2023-04-15 23:50:36 +0900

    feat: 간편하게 class 선택..

---
+++ main.js
@@ -0,0 +1,4 @@
+const container = document.querySelector(".image-container");
+const startButton = document.querySelector(".start-button");
+const gameText = document.querySelector(".game-text");
+const playTime = document.querySelector(".play-time");
