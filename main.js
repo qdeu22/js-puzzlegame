c84d91cc986e28951c81f8ff7a8bcb8d2f07bf97
qdeu22 <qdeu22@gmail.com>
2023-04-16 11:15:55 +0900

    feat: 함수를 이용해서 li 생성...

--- main.js
+++ main.js
@@ -11,14 +11,23 @@ const tileCount = 16;
 
 //Array(tileCount).fill(); //16개 짜리 배열 값은 안들어 있음..
 
-Array(tileCount)
-  .fill()
-  .forEach((_, i) => {
-    const li = document.createElement("li"); //새로운 HTML 요소를 생성하는 코드
-    li.setAttribute("data-index", i); //HTML 요소의 속성 값을 설정하는 코드
-    li.classList.add(`list${i}`); // HTML 요소의 클래스를 조작하는 코드
-    container.appendChild(li); //새로운 HTML 요소를 문서에 추가하는 코드
-  });
+let tiles = [];
+
+tiles = createImageTiles();
+console.log(tiles);
+function createImageTiles() {
+  const tempArray = [];
+  Array(tileCount)
+    .fill()
+    .forEach((_, i) => {
+      const li = document.createElement("li"); //새로운 HTML 요소를 생성하는 코드
+      li.setAttribute("data-index", i); //HTML 요소의 속성 값을 설정하는 코드
+      li.classList.add(`list${i}`); // HTML 요소의 클래스를 조작하는 코드
+      container.appendChild(li); //새로운 HTML 요소를 문서에 추가하는 코드
+      tempArray.push(li);
+    });
+  return tempArray;
+}
 
 function shuffle(array) {
   let index = array.length - 1; //배열 마지막값
